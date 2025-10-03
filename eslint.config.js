d04e23e0cfd8ce0cab4b430a44fb5c3571622e7c
Vayneel <alexander.mykyta@gmail.com>
2025-10-03 17:21:16 +0200

    fix: linter

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ...js.configs.recommended,
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "module",
+    },
+    rules: {
+      semi: ["error", "always"],
+      quotes: ["error", "single"],
+    },
+  },
+]);
